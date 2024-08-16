95f124df831ffbc732c88a746786772a18631350
Dean Kinyua <kinyuadean@gmail.com>
2024-08-16 11:04:45 +0300

    A function is tail recursive if the last thing executed by the function is a call to itself

--- lib/recursive/recursion.ex
+++ lib/recursive/recursion.ex
@@ -1,2 +1,8 @@
 defmodule Recursive.Recursion do
+  def upto(0), do: :ok
+  # note: A function is tail-recursive if the last thing executed by the function is a call to itself.
+  def upto(num) do
+    IO.puts("number #{num}")
+    upto(num - 1)
+  end
 end
