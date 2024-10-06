13dcd56614d0391875082f5cd226e3ef4ff55eaf
Dean Kinyua <kinyuadean@gmail.com>
2024-10-06 18:03:10 +0300

    a a

--- lib/recursive/streams.ex
+++ lib/recursive/streams.ex
@@ -12,6 +12,50 @@ defmodule Recursive.Streams do
     end)
   end
 
+  def large_lines do
+    path = Path.absname("mix.exs")
+
+    File.stream!(path)
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Enum.filter(&(String.length(&1) > 80))
+  end
+
+  def lines_length do
+    path = Path.absname("mix.exs")
+
+    File.stream!(path)
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Stream.map(&String.length(&1))
+    |> Enum.to_list()
+  end
+
+  def longest_line do
+    path = Path.absname("mix.exs")
+
+    file_list =
+      File.stream!(path)
+      |> Stream.map(&String.replace(&1, "\n", ""))
+
+    max_length =
+      file_list
+      |> Stream.map(&String.length(&1))
+      |> Enum.max()
+
+    long_string =
+      file_list
+      |> Enum.filter(&(String.length(&1) == max_length))
+      |> Enum.at(0)
+
+    # max_length
+    # Enum.to_list(file_list)
+    long_string
+  end
+
+  def read_file do
+    path = Path.absname("mix.exs")
+    File.read!(path)
+  end
+
   # In contrast, if you used Enum functions everywhere,
   # you would have to run multiple iterations over each intermediate list, which would
   # incur a performance and memory-usage penalty
