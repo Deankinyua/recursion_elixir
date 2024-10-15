61d9500477e802a2dbdb32cbc79a596e219300e9
Dean Kinyua <kinyuadean@gmail.com>
2024-10-15 13:31:39 +0300

    use structs when possible

--- lib/recursive/todolist.ex
+++ lib/recursive/todolist.ex
@@ -1,22 +1,40 @@
 defmodule Recursive.TodoList do
-  alias Recursive.MultiDict
+  defstruct auto_id: 1, entries: Map.new()
+  alias Recursive.TodoList
+  # alias Recursive.MultiDict
   # * in any complex system there will be need for higher level data abstractions
   # * in object oriented languages these abstractions are implemented via classes and objects
-  # * In Elixir, such abstractions are implemented with pure, stateless modules
+  # * In Elixir, such abstractions are implemented with pure, stateless modules jin form of structs
 
-  def new, do: MultiDict.new()
+  def new, do: %TodoList{}
 
-  def add_entry(todo_list, entry) do
-    MultiDict.add(todo_list, entry.date, entry)
+  # * %TodoList{entries: entries, auto_id: auto_id} = todo_list
+  # * requires that the todo_list be in the above form meaning has the entries and the auto_id value
+  def add_entry(
+        %TodoList{entries: entries, auto_id: auto_id} = todo_list,
+        entry
+      ) do
+    entry = Map.put(entry, :id, auto_id)
+    new_entries = Map.put(entries, auto_id, entry)
+    %TodoList{todo_list | entries: new_entries, auto_id: auto_id + 1}
   end
 
-  def entries(todo_list, date) do
-    MultiDict.get(todo_list, date)
+  # * %TodoList{entries: entries}
+  # * out of the todolist that is past only take the entries field and throw
+  # * away the auto_id field
+
+  def entries(%TodoList{entries: entries}, date) do
+    entries
+    |> Stream.filter(fn {_, entry} ->
+      entry.date == date
+    end)
+    |> Enum.map(fn {_, entry} ->
+      entry
+    end)
   end
 
-  def today(todo_list) do
-    {date, _time} = :calendar.local_time()
+  # todo = TodoList.new() |>
+  # TodoList.add_entry(%{date: {2013, 12, 19}, title: "Dentist"})
 
-    MultiDict.get(todo_list, date)
-  end
+  # {date, _time} = :calendar.local_time()
 end
